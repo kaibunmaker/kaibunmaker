6841b32a13333511c2899e8126e4d7fdd65b42c7
Munieru <20086673+munierujp@users.noreply.github.com>
2018-12-08 22:56:18 +0900

    Add AppOutput component

---
+++ components/AppOutput.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-textarea
+    :value="value"
+    readonly
+    outline
+    auto-grow
+    counter/>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
