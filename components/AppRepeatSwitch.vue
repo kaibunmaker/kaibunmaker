8f373c85d0999914ea1bf9f89344055796c0d21a
Munieru <20086673+munierujp@users.noreply.github.com>
2018-12-08 22:54:55 +0900

    Add AppRepeatSwitch component

---
+++ components/AppRepeatSwitch.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-checkbox
+    v-model="computedValue"
+    :label="$t('REPEAT_LAST_CHARACTER')"/>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    computedValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
