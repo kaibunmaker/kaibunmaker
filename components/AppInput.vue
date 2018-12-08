7acadfe459e455dc953aa8c029339676d21abbbd
Munieru <20086673+munierujp@users.noreply.github.com>
2018-12-08 22:46:53 +0900

    Add AppInput component

---
+++ components/AppInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-text-field
+    :value="value"
+    :label="$t('INPUT')"
+    solo
+    autofocus
+    @input="updateValue"/>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    updateValue (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
