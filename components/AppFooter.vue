129fae732b5d9c6dab5dfc60efd0e913004ef059
Munieru <20086673+munierujp@users.noreply.github.com>
2018-12-08 23:22:00 +0900

    Add AppFooter component

---
+++ components/AppFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-footer
+    app
+    color="grey darken-4"
+    class="app-footer grey--text text--lighten-5">
+    <v-spacer />
+    <div>
+      <i18n
+        tag="span"
+        path="FOOTER_CREATOR">
+        <a
+          :href="$t('FOOTER_CREATOR_URL')"
+          target="_blank">{{ $t('FOOTER_CREATOR_LABEL') }}</a>
+      </i18n>
+      <span>-</span>
+      <i18n
+        tag="span"
+        path="FOOTER_HOSTING_SITE">
+        <a
+          :href="$t('FOOTER_HOSTING_SITE_URL')"
+          target="_blank">{{ $t('FOOTER_HOSTING_SITE_LABEL') }}</a>
+      </i18n>
+    </div>
+    <v-spacer />
+  </v-footer>
+</template>
+
+<style lang="scss" scoped>
+.app-footer {
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+}
+</style>
