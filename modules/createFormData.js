a451c3e8341ee24ef55516574feccd291fc6bb69
Munieru <20086673+munierujp@users.noreply.github.com>
2018-12-17 14:18:14 +0900

    Add createFormData function

---
+++ modules/createFormData.js
@@ -0,0 +1,13 @@
+import Val from '@munierujp/val'
+import isPalindrome from '~/modules/isPalindrome'
+import parsePalindrome from '~/modules/parsePalindrome'
+
+export default function (value) {
+  return Val.of(value)
+    .filter(value => isPalindrome(value))
+    .map(palindrome => parsePalindrome(palindrome))
+    .or({
+      text: '',
+      repeat: false
+    })
+}
