5aa66fbbdd6ed11c5e1417b5c45a827ce934e49c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-08-18 08:53:33 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -19,22 +19,37 @@
 </template>
 
 <script>
+// @ts-check
+
 import createFormData from '../modules/createFormData'
 import createPalindrome from '../modules/createPalindrome'
 import createShareMessage from '../modules/createShareMessage'
+// @ts-ignore
 import AppFooter from '../components/AppFooter'
+// @ts-ignore
 import AppInput from '../components/AppInput'
+// @ts-ignore
 import AppOutput from '../components/AppOutput'
+// @ts-ignore
 import AppRepeatSwitch from '../components/AppRepeatSwitch'
+// @ts-ignore
 import AppTitle from '../components/AppTitle'
+// @ts-ignore
 import AppTweetButton from '../components/AppTweetButton'
 
+/** @type {Record<string, unknown>} */
+// @ts-ignore
 const config = process.env.config
+/** @type {string} */
+// @ts-ignore
 const ORIGIN = config.ORIGIN
+/** @type {string} */
+// @ts-ignore
 const BASE_DIR = config.BASE_DIR
 const BASE_URL = new URL(BASE_DIR, ORIGIN).toString()
 
 export default {
+  // @ts-ignore
   head () {
     return {
       link: [
@@ -52,27 +67,32 @@ export default {
   },
   computed: {
     appName () {
+      // @ts-ignore
       return this.$t('APP_NAME')
     },
     /**
      * @returns {string}
      */
     palindrome () {
+      // @ts-ignore
       const text = this.text
 
       if (typeof text !== 'string' || text === '') {
         return ''
       }
 
+      // @ts-ignore
       return createPalindrome(text, this.repeat)
     },
     shareTitle () {
+      // @ts-ignore
       return this.palindrome || this.appName
     },
     /**
      * @returns {string}
      */
     shareUrl () {
+      // @ts-ignore
       const palindrome = this.palindrome
 
       if (typeof palindrome !== 'string' || palindrome === '') {
@@ -84,17 +104,22 @@ export default {
       return url.toString()
     },
     shareMessageTemplate () {
+      // @ts-ignore
       return this.$t('SHARE_MESSAGE_TEMPLATE')
     },
     shareMessage () {
+      // @ts-ignore
       return createShareMessage(this.shareMessageTemplate, {
+        // @ts-ignore
         title: this.shareTitle,
+        // @ts-ignore
         url: this.shareUrl
       })
     }
   },
   methods: {
     updateText (text) {
+      // @ts-ignore
       this.text = text
     }
   },
@@ -103,7 +128,9 @@ export default {
   },
   beforeRouteUpdate (to, from, next) {
     const { text, repeat } = createFormData(to.query.status)
+    // @ts-ignore
     this.text = text
+    // @ts-ignore
     this.repeat = repeat
     next()
   }
