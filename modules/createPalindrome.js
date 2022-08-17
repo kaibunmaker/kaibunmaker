8dc78408f6b082ac788ed99c2ae7e14f01820420
Munieru <20086673+munierujp@users.noreply.github.com>
2022-08-18 00:23:39 +0900

    Update createPalindrome.js

--- modules/createPalindrome.js
+++ modules/createPalindrome.js
@@ -1,5 +1,13 @@
+// @ts-check
+
+// @ts-ignore
 import reverseString from '~/modules/reverseString'
 
+/**
+ * @param {string} text
+ * @param {boolean} repeat
+ * @returns {string}
+ */
 export default function (text, repeat) {
   const reversedText = reverseString(text)
   return repeat ? `${text}${reversedText}` : `${text.substring(0, text.length - 1)}${reversedText}`
