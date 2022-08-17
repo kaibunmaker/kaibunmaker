dd4f3099ddd1dc145d6ed149e8fa09262bb4e65d
Munieru <20086673+munierujp@users.noreply.github.com>
2022-08-18 08:41:29 +0900

    Update JSDoc

--- modules/createShareMessage.js
+++ modules/createShareMessage.js
@@ -5,7 +5,7 @@ import { render } from 'mustache'
 /**
  * @param {string} template
  * @param {{ title: string, url: string }} data
- * @returns
+ * @returns {string}
  */
 export default function (template, data) {
   return render(template, {
