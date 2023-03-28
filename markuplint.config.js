aa551008735f640964f06ae989ce2e430c7bedde
Munieru <20086673+munierujp@users.noreply.github.com>
2023-03-29 01:02:41 +0900

    Introduce Markuplint

---
+++ markuplint.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+/** @typedef {import('@markuplint/ml-config').Config} Config */
+
+/**
+ * @type {Config}
+ * @see https://markuplint.dev/docs/configuration
+ */
+const config = {
+  extends: [
+    'markuplint:recommended'
+  ],
+  parser: {
+    '\\.vue$': '@markuplint/vue-parser'
+  },
+  specs: {
+    '\\.vue$': '@markuplint/vue-spec'
+  }
+}
+
+module.exports = config
